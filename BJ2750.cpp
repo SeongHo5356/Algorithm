b65430a65161a92383d867d6b31691a73cf91f63
Your Name <kjeng7897@gmail.com>
2023-11-14 16:18:14 +0900

    Baekjoon2750

--- BJ2750.cpp
+++ BJ2750.cpp
@@ -1,39 +1,55 @@
 #include <stdio.h>
 
+int data[1001];
+
 int main(){
-	int number;
+	int number, temp;
 	scanf("%d", &number);
-	int data[number];
-	for(int i = 0; i < number; i++){
-		scanf("%d", &data[i]);
+	for(int i=0;i<number;i++){
+		scanf("%d",&data[i]);
 	}
 	
-	for(int i = 0 ; i<number; i++){
-		for(int j = i; j > 0; j--){
-			if (data[j] < data[j-1] && j-1 > 0 ){
-				int temp = data[j];
-				data[j] = data[j-1];
-				data[j-1] = data[j];
+	//Selection Sort
+/*	for(int i =0;i<number; i++){
+		int index = i;
+		for(int k =i+1; k<number; k++){
+			if(data[index]> data[k]){
+				index = k;
 			}
 		}
-			
-	}
-	
-/*	for(int i = number - 1; i >= 0; i--){
-		for(int j = 0; j < i; j++){
-			if(data[j]>data[j+1]){
-				int temp = data[j];
-				data[j] = data[j+1];
-				data[j+1] = temp;
-			}
+		if(i != index){
+			temp = data[i];
+			data[i] = data[index];
+			data[index] = temp;
 		}
 	}*/
+	
+	//Bubble Sort
+/*	for(int i =0; i<number-1; i++){
+		int j = i;
+		while(j>=0 && data[j]>=data[j+1]){
+			temp = data[j+1];
+			data[j+1] = data[j];
+			data[j] = temp;
+			j--;
+		}
+	}*/
+	
+	//Insertion Sort
+	for(int i =0; i< number; i++){
+		for(int j = 0; j<number-1-i; j++){
+			if(data[j]>data[j+1]){
+				temp = data[j+1];
+				data[j+1] = data[j];
+				data[j] = temp;
+			}
+		}
+	}
+	
 	
 	for(int i =0; i<number; i++){
 		printf("%d ", data[i]);
 	}
-	
-	
-	
+		
 	return 0;
 }
