b57cb9ad7b16f5815cbb54c091e1f13787700405
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-04-18 01:11:00 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -0,0 +1,10 @@
+def solution(wallpaper):
+    x = []
+    y = []
+    for i in range(len(wallpaper)):
+        for k in range(len(wallpaper[i])):
+            if wallpaper[i][k] == '#':
+                x.append(i)
+                y.append(k)
+    answer = [min(x), min(y), max(x)+1, max(y)+1]
+    return answer
