5bb506a5d03973721c8c860e8663d49b96d03640
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-04-21 01:09:27 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,9 @@
+def solution(array, commands):
+    answer = []
+    for i in range(len(commands)):
+        new_arry = array[commands[i][0]-1 : commands[i][1]]
+        new_arry.sort()
+        N = commands[i][2]
+        trgt = new_arry[N-1]
+        answer.append(trgt)
+    return answer
