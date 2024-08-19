0b7b0d4d60acdffa73e8006e5d4df79bece841f7
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-08-20 02:36:29 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+N, K = map(int, input().split())
+
+circle = deque()
+answer = []
+
+for i in range(N):
+    circle.append(i+1)
+
+while circle:
+    circle.rotate(1-K)
+    popNum = circle.popleft()
+    answer.append(popNum)
+
+print(str(answer).replace('[', '<').replace(']', '>'))
