58883004d185ffe45a71d7ee5bf28d520ea0197d
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-08-24 01:08:54 +0900

    [level 2] Title: 주식가격, Time: 201.05 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,11 +1,9 @@
 def solution(prices):
     answer = []
-    prices = prices
     for i in range(len(prices)):
         cnt = 0
         for k in range(len(prices)-(i+1)):   # i=0. range(5-1), # i=1. range(5-2)
-            cnt += 1
-            if prices[i] > prices[k+i+1]:
-                break
+            if prices[i] <= prices[k+i+1]:
+                cnt += 1
         answer.append(cnt)
     return answer
