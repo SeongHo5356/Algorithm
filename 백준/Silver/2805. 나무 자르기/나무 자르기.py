b9963bb3515fb31bb73a5675bbebde15cc4155ce
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-09-10 18:43:31 +0900

    [Silver II] Title: 나무 자르기, Time: 2368 ms, Memory: 146420 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -8,23 +8,28 @@ mx_wood_length = max(woods)
 def cut_woods(a, woods):
     sum = 0
     for wood in woods:
+        
         if wood - a <= 0:
             continue
         else :
+            
             sum += (wood - a) 
+
     return sum
 
-def binary_search(woods, M):
+def bnry_srch(woods, M):
     start, end = 0, mx_wood_length
     result = 0
     while start <= end:
-        mid = (start + end) // 2
+        mid = (start + end)//2
         if cut_woods(mid, woods) >= M:
             result = mid
             start = mid + 1
         else:
             end = mid - 1
+
     return result
 
-answer = binary_search(woods, M)
-print(answer)
+정답 = bnry_srch(woods, M)
+print(정답)
+
