14dcba85cbd4f47d9b593e934277741852551f27
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-09-17 02:41:22 +0900

    [level 2] Title: H-Index, Time: 0.14 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,6 @@
  
 
-def solution(citations):
+def solution_2(citations):
     answer = 0
     citations.sort(reverse=True)
     
@@ -9,3 +9,24 @@ def solution(citations):
             return i
 
     return len(citations) 
+
+def solution_3(citations):
+    mx = max(citations)
+    ans = 0
+    for i in range(1, mx+1):
+        H_idx = 0
+        for citation in citations:
+            if citation >= i:
+                H_idx += 1
+        if H_idx == i:
+            if ans <= H_idx:
+                ans = H_idx
+        
+    return ans
+
+def solution(citations):
+    citations.sort(reverse=True)
+    for i, citation in enumerate(citations, 1):
+        if citation < i:
+            return i - 1
+    return len(citations)
