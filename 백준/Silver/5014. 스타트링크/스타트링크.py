9cc4378d352ab458b871a7dd8719eb1209b08b9c
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-09-27 23:32:24 +0900

    [Silver I] Title: 스타트링크, Time: 352 ms, Memory: 41440 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -1,32 +1,26 @@
 from collections import deque
+F,S,G,U,D = map(int, input().split())
 
-def bfs(F, S, G, U, D):
-    queue = deque([(S, 0)])  # (현재 층, 버튼 클릭 횟수)
-    visited = [False] * (F + 1)  # 방문한 층 체크
-    visited[S] = True
+min_n = [float('inf')]
+v = [0]*(F+1)
 
-    while queue:
-        current_floor, clicks = queue.popleft()
+def dfs(F, S, G, U, D):
+    q = deque([(S, 0)])
+    v[S] = 1
+    
+    while q:
+        ci, cnt = q.popleft()
+               
+        if ci == G:
+            return cnt
 
-        if current_floor == G:
-            return clicks
-
-        up_floor = current_floor + U
-        down_floor = current_floor - D
-
-        if up_floor <= F and not visited[up_floor]:
-            queue.append((up_floor, clicks + 1))
-            visited[up_floor] = True
-
-        if down_floor >= 1 and not visited[down_floor]:
-            queue.append((down_floor, clicks + 1))
-            visited[down_floor] = True
-
+        for di in [U, -D]:
+            ni = ci + di
+            if 1<=ni<=F and v[ni] == 0:
+                v[ni] = 1
+                q.append((ni, cnt+1))
+                
     return "use the stairs"
 
-# 입력 받기
-F, S, G, U, D = map(int, input().split())
-
-# 결과 출력
-result = bfs(F, S, G, U, D)
-print(result)
+print(dfs(F, S, G, U, D))
+    
