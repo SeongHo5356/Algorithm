7a8889caa2b25faa7bc89f358a757d89c5cb019f
Your Name <kjeng7897@gmail.com>
2024-09-27 21:55:04 +0900

    ADD:BJ1697

---
+++ milla/BJ1697_2.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+N, K = map(int, input().split())
+v = [0]*(100000*2+1)
+
+def bfs(si, ei):
+    q = deque([(si, 0)])
+    v[si] = 1
+    while q:
+        ci, cnt = q.popleft()
+        print("ci:{}, cnt:{}".format(ci, cnt))
+        if ci == ei:
+            return cnt
+        
+        for di in (ci*2, ci+1, ci-1):
+            ni = di
+            if 0<=ni<(100000*2+1) and v[ni]==0:
+                v[ni] = 1
+                q.append((ni, cnt+1))
+    
+    return cnt
+
+print(bfs(N,K))
