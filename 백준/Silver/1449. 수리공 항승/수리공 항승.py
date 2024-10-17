47937523728d42a5dcbf7ae8023a2cdf10f8eab5
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2024-10-17 20:48:53 +0900

    [Silver III] Title: 수리공 항승, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -1,24 +1,19 @@
-def min_tape_count(n, l, leak_positions):
-    # 물이 새는 위치들을 오름차순으로 정렬
-    leak_positions.sort()
-    
-    # 필요한 테이프의 개수
-    tape_count = 0
-    # 현재 테이프가 덮을 수 있는 마지막 위치
-    current_end = 0
+def min_tapes(N, L, leaks):
+    leaks.sort()  # 물이 새는 위치를 오름차순으로 정렬
+    count = 1
+    end = leaks[0] + L - 0.5  # 첫 번째 테이프의 끝 위치
 
-    for leak in leak_positions:
-        # 만약 현재 위치가 현재 테이프가 덮을 수 있는 범위 밖이면, 새로운 테이프 필요
-        if leak > current_end:
-            # 새로운 테이프를 붙이고, 그 테이프가 덮는 범위 계산
-            tape_count += 1
-            current_end = leak - 0.5 + l
-    
-    return tape_count
+    for i in range(1, N):
+        if leaks[i] > end:  # 다음 물이 새는 위치가 현재 테이프 범위를 벗어나면
+            count += 1
+            end = leaks[i] + L - 0.5
 
-# 입력 받기
-n, l = map(int, input().split())
-leak_positions = list(map(int, input().split()))
+    return count
 
-# 결과 출력
-print(min_tape_count(n, l, leak_positions))
+# 예제 입력
+N, L = map(int, input().split())
+leaks = list(map(int, input().split()))
+
+# 풀이 및 출력
+result = min_tapes(N, L, leaks)
+print(result)
