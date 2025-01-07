97058135b5f5da5ee91a47dbf6ffa545354777d5
SeongHo5356 <65662139+SeongHo5356@users.noreply.github.com>
2025-01-07 20:51:09 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -5,3 +5,11 @@ join USED_GOODS_REPLY B
 on A.BOARD_ID = B.BOARD_ID
 where YEAR(A.CREATED_DATE) = '2022' and MONTH(A.CREATED_DATE) = '10'
 order by B.CREATED_DATE asc, A.TITLE asc;
+
+
+
+select a.title, a.board_id, b.reply_id, b.writer_id, b.contents, date_format(b.created_date, '%Y-%m-%d') as created_date
+from used_goods_board a
+join used_goods_reply b on a.board_id = b.board_id
+where year(a.created_date) = '2022' and month(a.created_date) = '10'
+order by b.created_date asc, a.title asc;
