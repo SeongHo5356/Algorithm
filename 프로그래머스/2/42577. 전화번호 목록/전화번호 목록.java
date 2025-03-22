d86276d127960fb171eb292736baad5c30407172
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-03-23 01:38:41 +0900

    [level 2] Title: 전화번호 목록, Time: 363.99 ms, Memory: 200 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,17 +4,20 @@ import java.util.Set;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
+        
         // 1. HashSet을 선언한다.
         Set<String> set = new HashSet<>();
         
-        // 2. 모든 전화번호를 HashMap에 넣는다.
+        // 2. 모든 전화번호를 HashSet에 넣는다.
         for (String phone : phone_book){
             set.add(phone);
         }
         
+        // 3. 각 전화번호의 접두어가 다른 번호에 포함되어 있는지 확인한다.
         for (String phone : phone_book) {
-            for(int i=0; i<phone.length(); i++){
-                if(set.contains(phone.substring(0,i))){
+            for(int i=1; i<phone.length(); i++){
+                // 접두어 substring(0, i)이 다른 전체 번호에 존재하면 접두어 관계이므로 false 반환
+                if(set.contains(phone.substring(0, i))){
                     answer = false;
                 }
             }
