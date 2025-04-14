d0103e5b3d1abc7895aaa7c6a126ac835d47e6cb
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-14 21:44:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 81.54 ms, Memory: 25.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 95.5 MB, 시간: 75.02 ms
+메모리: 25.3 MB, 시간: 81.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 23일 13:03:39
+2025년 04월 14일 21:44:30
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,9 +1,27 @@
+def solution2(participant, completion):
+    participant.sort()
+    completion.sort()
+    answer = participant[-1]
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            answer = participant[i]
+            break
+    return answer
+
+
+
+
+
+
+
+
+
 def solution(participant, completion):
     participant.sort()
     completion.sort()
-    
-    for i in range (0,len(completion)):
-        if participant[i] != completion[i]:
+    for i in range(len(completion)):
+        if completion[i] != participant[i]:
             return participant[i]
-        
-    return participant[-1]
+    
+    return participant.pop()
+            
