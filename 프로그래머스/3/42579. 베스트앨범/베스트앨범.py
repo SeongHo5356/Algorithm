8b2327de340b00d45747438fcc2d6f4fb9f6ed16
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-15 01:40:59 +0900

    [level 3] Title: 베스트앨범, Time: 0.12 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,25 @@
+def solution(genres, plays):
+    answer = []    
+    # [장르, 재생횟수, 고유 번호] 리스트
+    temp = [[genres[i], plays[i], i] for i in range(len(genres))]
+    
+    # 각 장르에 맞게 정렬하고, 많이 재생될수록, 고유번호가 낮을 수록
+    temp = sorted(temp, key=lambda x:(x[0], -x[1], x[2])) 
+    
+    dict_count = {}
+    for i in range(len(genres)):
+        if genres[i] in dict_count:
+            dict_count[genres[i]] += plays[i] 
+        else :
+            dict_count[genres[i]] = plays[i]
+    
+    dict_count = sorted(dict_count.items(), key=lambda x: -x[1])
+    for key in dict_count :
+        cnt = 0
+        for tp in temp:
+            if tp[0] == key[0] and cnt <= 1:
+                answer.append(tp[2])
+                cnt += 1
+    print(answer)
+    
+    return answer
