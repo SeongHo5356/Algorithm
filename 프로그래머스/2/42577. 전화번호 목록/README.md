8c22814741fb81af319fbc5584885b81fd25c5ed
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-14 22:25:21 +0900

    [level 2] Title: 전화번호 목록, Time: 99.98 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
 
 ### 성능 요약
 
-메모리: 200 MB, 시간: 363.99 ms
+메모리: 30.1 MB, 시간: 99.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 23일 01:38:39
+2025년 04월 14일 22:25:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,7 +1,33 @@
-def solution(phone_book):
+def solution1(phone_book):
     answer = True
     phone_book.sort()
     for i in range(len(phone_book)-1):
         if phone_book[i+1][:len(phone_book[i])] == phone_book[i]:
             answer = False
     return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1][:len(phone_book[i])] == phone_book[i]:
+            return False
+    return True
+
+
+
