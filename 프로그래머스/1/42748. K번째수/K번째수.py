2613dc4271324d8dd720e92d7ed7392df7ab0a11
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-15 15:13:59 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,32 @@
-def solution(array, commands):
+def solution1(array, commands):
     answer = []
     for i in range(len(commands)):
         new_arry = array[commands[i][0]-1 : commands[i][1]]
         new_arry.sort()
         trgt = new_arry[commands[i][2]-1]
         answer.append(trgt)
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def solution(array, commands):
+    answer = []
+    for start, end, idx in commands:
+        tmp = sorted(array[start-1:end], key=lambda x : x)
+        answer.append(tmp[idx-1])
+    
     return answer
