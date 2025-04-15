ef8e6f33b2c9d3739b4dde00423a540bbff85232
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-15 15:29:39 +0900

    [level 2] Title: 가장 큰 수, Time: 72.96 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,9 +1,45 @@
-def solution(numbers):
+def solution_2(numbers):
     answer = 0
     tmp = []
     for num in numbers :
         tmp.append(str(num))
     tmp.sort(key = lambda x : x*3,reverse = True)
     answer = str(int(''.join(tmp)))
+    
+    return answer
+
+import itertools
+
+def solution1(numbers):
+    numbers = list(itertools.permutations(numbers, len(numbers)))
+    max = 0
+    for number in numbers:
+        curr = int(''.join(map(str,number)))
+        if max < curr: max = curr
+    return str(max)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def solution(numbers):
+    answer = ""
+    numbers_str = sorted(numbers, key=lambda x: str(x)*3, reverse=True)
+    for number in numbers_str:
+        answer += str(number)
+        
+    if answer[0] == "0":
+        return "0"
     
     return answer
