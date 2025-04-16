14a6fdcf5a9056d7dc18749617849adc45d89db2
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-16 12:49:37 +0900

    [level 2] Title: 타겟 넘버, Time: 240.03 ms, Memory: 49.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,56 +1,14 @@
-from collections import deque
-
-def solution_1(numbers, target):
-    # 아직 전체 합이 0이고, 돈 횟수 0 
-    q = deque([(0, 0)]) 
-    cnt = 0
-    
-    for i in range(len(numbers)):
-        for j in range(2**i):
-            cur, itr = q.popleft()    
-            q.append((cur+numbers[i],itr+1))
-            q.append((cur-numbers[i],itr+1))
-            
-    
-    print(q)
-    for node1, node2 in q:
-        if node1 == target and node2 == len(numbers):
-            cnt += 1
-    
-    
-    return cnt 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def DFS(numbers, target, depth):
-    answer = 0
-    if depth == len(numbers):
-        if sum(numbers) == target : 
-            return 1
-        else :
-            return 0
-    else:
-        answer += DFS(numbers, target, depth+1)
-        numbers[depth] *= -1
-        answer += DFS(numbers, target, depth+1)
-    
-    return answer
-        
 def solution(numbers, target):
-    answer = DFS(numbers, target, 0)
-    return answer
-    
-    
+    ans = 0
+    leaves = [0]
+    for number in numbers:
+        tmp = []
+        for parent in leaves:
+            tmp.append(parent + number)
+            tmp.append(parent - number)
+        leaves = tmp
+        
+    for leave in leaves:
+        if leave == target:
+           ans += 1
+    return ans 
