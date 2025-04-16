51ec7fdb3152dc1cfea72b00375b560ab9822216
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-16 16:11:07 +0900

    [level 3] Title: 네트워크, Time: 3.28 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.77 ms
+메모리: 9.45 MB, 시간: 3.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 26일 02:23:38
+2025년 04월 16일 16:11:05
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -6,7 +6,7 @@ def ZeroSelf(computers):
     return computers
     
     
-def solution(n, computers):
+def solution_1(n, computers):
     visited = [0]*n
     computers = ZeroSelf(computers)
     cnt = 0
@@ -29,4 +29,47 @@ def solution(n, computers):
     
     return cnt 
 
-solution(3,	[[1, 1, 0], [1, 1, 0], [0, 0, 1]])
+
+'''
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    
+    for i in range(n):
+        if visited[i] == False:
+            #### 파이썬에서 mutable 객체를 인자로 전달하면 무조건 참조에 의한 전달이 일어남
+            dfs(n, computers, i, visited)
+            answer += 1
+    return answer
+
+def dfs(n, computers, now, visited):
+    visited[now] = True
+    for i in range(n):
+        if i != now and visited[i] == False and computers[i][now]:
+            dfs(n, computers, i, visited)
+'''
+
+
+def solution(n, computers):
+    answer = 0
+    visited = [False for _ in range(n)]
+    
+    for i in range(n):
+        if visited[i] == False:
+            #### 파이썬에서 mutable 객체를 인자로 전달하면 무조건 참조에 의한 전달이 일어남
+            bfs(n, computers, i, visited)
+            answer += 1
+    return answer
+
+def bfs(n, computers, now, visited):
+    visited[now] = True
+    queue = []
+    queue.append(now)
+    
+    while queue:
+        com = queue.pop(0)
+        visited[com] = True
+        for i in range(n):
+            if i != com and visited[i] == False and computers[com][i] == 1:
+                queue.append(i)
+        
