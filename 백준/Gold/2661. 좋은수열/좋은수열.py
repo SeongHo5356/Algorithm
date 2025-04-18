ff4d9bc05620607fbbe96f0be39c04cf62158bb0
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-19 01:43:40 +0900

    [Gold IV] Title: 좋은수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -1,21 +1,22 @@
-def back_tracking(idx):
-    #1
-    for i in range(1,(idx//2) +1):
-        if s[-i:] == s[-2 * i:-i]: 
-            return -1
-    
-    if idx == n:
-        for i in range(n):
-            print(s[i], end='')
-        return 0
+N = int(input())
+ans = []
+def check(num):
+    for i in range(1, len(num)//2+1):
+        if num[-i:] == num[-i*2:-i]:
+            return False
+    return True
 
-    #3
-    for i in range(1, 4):
-        s.append(i)
-        if back_tracking(idx + 1) == 0:
-            return 0
-        s.pop()
+def dfs(num):
+    if len(num) == N and check(num) == True:
+        print(num)
+        return True
 
-n = int(input())
-s = []
-back_tracking(0)
+    for char in ['1', '2', '3']:
+        if check(num + char):
+            if dfs(num+char):
+                return True
+    return False
+
+
+dfs('')
+
