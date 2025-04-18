0d8350aa92b7e8913c4cf3ed43d405d3ef5f9b57
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-18 20:55:15 +0900

    [Silver III] Title: N과 M (3), Time: 1112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,14 +1,15 @@
-n,m= map(int,input().split())
- 
-s = []
- 
+N, M = map(int, input().split())
+
+ans = []
+
 def dfs():
-    if len(s)==m:
-        print(' '.join(map(str,s)))
+
+    if len(ans) == M:
+        print(' '.join(map(str, ans)))
         return
-    
-    for i in range(1,n+1):
-        s.append(i)
+
+    for i in range(1, N+1):
+        ans.append(i)
         dfs()
-        s.pop()
+        ans.pop()
 dfs()
