de30b2e68ff5015aeb2923925c7f82fde9ba346e
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-04-19 17:16:06 +0900

    [Silver I] Title: 음식물 피하기, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.py
@@ -1,36 +1,40 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
-N, M, K = map(int, input().split())
-board = [[0]*(M+1) for _ in range(N+1)]
-for _ in range(K):
-    r, c = map(int, input().split())
-    board[r][c] = 1
+N,M,K = map(int, input().split())
+Blocks = [[0]*(M+1) for _ in range(N+1)]
+V = [[False]*(M+1) for _ in range(N+1)]
+StartPoints = []
 
-visited = [[False]*(M+1) for _ in range(N+1)]
+for i in range(K):
+    cy,cx = map(int, input().split())
+    StartPoints.append([cy,cx])
+    Blocks[cy][cx] = 1
 
-def bfs(sy, sx):
-    q = deque([(sy, sx)])
-    visited[sy][sx] = True
+def bfs(sy,sx):
     size = 1
+    q = [[sy, sx]]
+    V[sy][sx] = True
 
     while q:
-        y, x = q.popleft()
-        for dy, dx in [(1,0),(-1,0),(0,1),(0,-1)]:
-            ny, nx = y + dy, x + dx
-            if 1 <= ny <= N and 1 <= nx <= M \
-               and board[ny][nx] == 1 and not visited[ny][nx]:
-                visited[ny][nx] = True
-                q.append((ny, nx))
+        y,x = q.pop(0)
+        for dy,dx in [(1,0), (-1,0), (0,1), (0,-1)]:
+            if 0 < y + dy <= N and 0 < x + dx <= M and Blocks[y + dy][x + dx] == 1 and V[y + dy][x + dx] == False:
+                V[y + dy][x + dx] = True
+                q.append([y+dy, x+dx])
                 size += 1
 
     return size
 
-max_size = 0
-for i in range(1, N+1):
-    for j in range(1, M+1):
-        if board[i][j] == 1 and not visited[i][j]:
-            max_size = max(max_size, bfs(i, j))
+ans = 0
+for sy, sx in StartPoints:
+    if V[sy][sx] == False:
+        ans = max(ans, bfs(sy,sx))
 
-print(max_size)
+print(ans)
+
+
+
+
+
+
