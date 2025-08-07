7f1fc0a8f7e772908f49b747d1fd5e4dd73595ca
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-08-07 13:44:36 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+
+        int cnt = 1;
+        int sum = 1;
+
+        while (N > sum) {
+            sum = sum + 6 * cnt;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
