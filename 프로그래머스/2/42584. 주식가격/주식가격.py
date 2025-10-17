e19f5830ceaa50bcc0b748960274f60e848efa6a
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-10-17 21:36:39 +0900

    [level 2] Title: 주식가격, Time: 236.20 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,9 +1,33 @@
-def solution(prices):
+def solution_1016(prices):
     answer = []
     for i in range(len(prices)):
         cnt = 0
         for k in range(len(prices)-(i+1)):   # i=0. range(5-1), # i=1. range(5-2)
-            if prices[i] <= prices[k+i+1]:
-                cnt += 1
+            cnt += 1
+            if prices[i] > prices[k+i+1]:
+                break
         answer.append(cnt)
     return answer
+
+
+def solution(prices):
+    length = len(prices)
+    solution = [0] * length
+    for i in range(length):
+        for j in range(i+1, length):
+            solution[i] += 1
+            if prices[i] > prices[j]:
+                break
+    return solution
+
+
+
+
+
+
+
+
+
+
+
+
