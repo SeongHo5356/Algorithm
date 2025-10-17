6f3efd829e302933dbe579fc6537be87c604cbfd
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-10-17 17:48:49 +0900

    [level 2] Title: H-Index, Time: 0.25 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -54,14 +54,13 @@ def solution_5(citations):
 
 
 
-
-
-
-
-
 def solution(citations):
-    citations.sort(reverse=True)
+    citations.sort(reverse = True)
+    h = 0
     for i in range(len(citations)):
-        if citations[i] < i+1:
-            return i
-    return len(citations)
+        if citations[i] >= i + 1:
+            h = i + 1
+        else :
+            break
+    return h
+        
