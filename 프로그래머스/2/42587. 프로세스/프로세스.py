d01b6fdfed0fa38fb33b981a0fd9771ca349d0df
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-11-08 01:26:04 +0900

    [level 2] Title: 프로세스, Time: 2.07 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,25 +1,34 @@
 from collections import deque
 
-def solution(pri, location):
-    pri_deque = deque(pri)
-    order = 0
+def solution(priorities, location):
+    answer = [] 
+    queue = []
     
-    while pri_deque:
-        if pri_deque[0] < max(pri_deque):
-            pri_deque.append(pri_deque.popleft())
-            location -= 1
-            if location == -1 :
-                location = len(pri_deque) - 1
-            
-        else :
-            pri_deque.popleft()
-            
-            if location == 0 :
-                order += 1 
-                break
-            else :
-                location -= 1
-                order += 1
-            
-            
-    return order 
+    for i in range(len(priorities)):
+        queue.append([priorities[i], i])
+    
+    count = 1
+    while(True):
+        # print("queue", queue)
+        cur = queue.pop(0)
+        # print("popped queue", queue)
+        # print("cur", cur)
+        
+        if cur[1] == location and cur[0] >= calculateMax(queue):
+            # print("ans", cur)
+            # print("count", count)
+            return count
+        
+        if cur[0] >= calculateMax(queue):
+            # print("cur[0]", "calculateMax(queue)", cur[0], calculateMax(queue))
+            count += 1
+        
+        else:
+            queue.append(cur)
+
+def calculateMax(queue):
+    max = 0
+    for i in range(len(queue)):
+        if max < queue[i][0]:
+            max = queue[i][0]
+    return max
