073779dbeaa15f0df7a8a09030c72082f616e320
SungHo Jung <65662139+SeongHo5356@users.noreply.github.com>
2025-11-21 12:46:51 +0900

    [Silver I] Title: 쿼드트리, Time: 104 ms, Memory: 111456 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,15 +1,40 @@
-
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
-board = [list(input().strip()) for _ in range(N)]   # 문자열로 저장
 
-def quad(cx, cy, size):
-    head = board[cx][cy]
+board = [list(input().strip()) for _ in range(N)]
+
+# def quad(cx, cy, size):
+#     head = board[cx][cy]
+#     is_same = True
+#     for i in range(cx, cx + size):
+#         for j in range(cy, cy + size):
+#             if board[i][j] != head:
+#                 is_same = False
+#                 break
+#         if not is_same:
+#             break
+#
+#     if is_same:
+#         return head
+#
+#     half = size // 2
+#     return "(" + \
+#         quad(cx, cy, half) + \
+#         quad(cx, cy + half, half) + \
+#         quad(cx + half, cy, half) + \
+#         quad(cx + half, cy + half, half) + \
+#         ")"
+#
+# print(quad(0,0,N))
+
+def quad(x,y,size):
+    head = board[x][y]
     is_same = True
-    for i in range(cx, cx + size):
-        for j in range(cy, cy + size):
+    for i in range(x, x+size):
+        for j in range(y, y+size):
             if board[i][j] != head:
                 is_same = False
                 break
@@ -21,15 +46,11 @@ def quad(cx, cy, size):
 
     half = size // 2
     return "(" + \
-        quad(cx, cy, half) + \
-        quad(cx, cy + half, half) + \
-        quad(cx + half, cy, half) + \
-        quad(cx + half, cy + half, half) + \
+        quad(x,y,half) + \
+        quad(x, y + half, half) + \
+        quad(x + half, y, half) + \
+        quad(x + half, y + half, half) + \
         ")"
 
 print(quad(0,0,N))
-
-
-
-
 
